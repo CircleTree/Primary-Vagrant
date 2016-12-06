62daf20aa0c60bcc21e257387df3aef7dd058974
bobbravo2 <bob@mycircletree.com>
2016-12-06 09:31:49 -0500

    Added SQL mode script added mysql database dump added index for Circle Tree home updated .gitignore to include our custom home index

---
+++ default-sites/pv/custom/index.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+	<title>Primary Vagrant</title>
+</head>
+<body>
+
+<h1>Welcome to Circle Tree's Primary Vagrant</h1>
+
+
+
+<h2>Our dev site(s)</h2>
+<ul>
+	<li><a href="http://mycircletree.pv/" target="_blank" >mycircletree.pv</a></li>
+</ul>
+
+<h2>Primary Vagrant Tools</h2>
+
+<ul>
+	<li><a href="http://phpmyadmin.pv" target="_blank">Manage MySQL with phpMyAdmin</a></li>
+	<li><a href="http://replacedb.pv" target="_blank">Easily Search and Replace MySQL database values with Search and Replace DB</a> (this makes migrating sites a breeze)</li>
+	<li><a href="http://webgrind.pv" target="_blank">Profile your application with Webgrind</a></li>
+	<li><a href="http://pv:8025" target="_blank">Collect email with MailHog</a></li>
+</ul>
+
+<h2>Development Sites</h2>
+
+<ul>
+	<li><a href="http://core.wordpress.pv" target="_blank">WordPress Core (src folder for WordPress core development)</a></li>
+	<li><a href="http://legacy.wordpress.pv" target="_blank">WordPress Legacy Branch (old version - 4.4.x)</a></li>
+	<li><a href="http://stable.wordpress.pv" target="_blank">WordPress Stable Branch (current version - 4.5.x)</a></li>
+	<li><a href="http://trunk.wordpress.pv" target="_blank">WordPress Trunk (development) Version</a></li>
+</ul>
+
+<h2>About Primary Vagrant</h2>
+<ul>
+	<li><a href="https://github.com/ChrisWiegman/Primary-Vagrant" target="_blank">View the Primary Vagrant Project on GitHub</a></li>
+</ul>
+
+</body>
+</html>
